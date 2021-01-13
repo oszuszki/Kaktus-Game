27c41469702f362153cc7218984641655d62a068
Oszuszki Richárd <oszuszki.richard@gmail.com>
2021-01-13 20:06:13 +0200

    Update player control system

--- src/com/game/entities/Player.java
+++ src/com/game/entities/Player.java
@@ -9,8 +9,8 @@ import java.awt.event.KeyEvent;
 
 public class Player {
 
-    private static int x = 100;
-    private static int y = 100;
+    private static int x = 60;
+    private static int y = 400;
     private static final int height = 64;
     private static final int width = 64;
     private static boolean right = false;
@@ -18,18 +18,18 @@ public class Player {
     private static boolean up = false;
     private static boolean down = false;
 
-    private static final double jumpSpeed = 5;
+    private static final double jumpSpeed = 8;
     private static double currentJumpSpeed = jumpSpeed;
     private static final boolean topCollision = false;
-    private static final double maxFallSpeed = 5;
-    private static double currentFallSpeed = 0.5;
+    private static final double maxFallSpeed = 8;
+    private static double currentFallSpeed = 0.3;
 
     public static void move() {
 
         if(MapBuilding.map1[((y) / 32)][((x + 68) / 32)] == 1 || MapBuilding.map1[((y) / 32) + 1][((x + 68) / 32)] == 1) {
             right = false;
         }
-        if(MapBuilding.map1[((y) / 32)][((x - 8) / 32)] == 1 || MapBuilding.map1[((y) / 32 + 1)][((x - 4) / 32)] == 1) {
+        if(MapBuilding.map1[((y) / 32)][((x - 6) / 32)] == 1 || MapBuilding.map1[((y) / 32 + 1)][((x - 6) / 32)] == 1) {
             left = false;
         }
         if(MapBuilding.map1[((y - 8) / 32)][(x / 32)] == 1 || MapBuilding.map1[((y - 8) / 32)][(x / 32) + 1] == 1) {
@@ -37,7 +37,7 @@ public class Player {
             down = true;
         }
         if(MapBuilding.map1[((y + 64) / 32)][((x) / 32)] == 1 || MapBuilding.map1[((y + 64) / 32)][((x) / 32) + 1] == 1
-                || MapBuilding.map1[((y + 64) / 32)][((x) / 32) + 2] == 1) {
+                || MapBuilding.map1[((y + 64) / 32)][((x) / 32) + 2] == 1 || MapBuilding.map1[((y + 64) / 32)][((x) / 32) - 1] == 1) {
             down = false;
         }  else {
             if (!topCollision && !up) {
@@ -57,7 +57,7 @@ public class Player {
 
         if (up) {
             y -= currentJumpSpeed;
-            currentJumpSpeed -= .1;
+            currentJumpSpeed -= .2;
             if (currentJumpSpeed <= 0) {
                 currentJumpSpeed = jumpSpeed;
                 up = false;
@@ -68,11 +68,11 @@ public class Player {
         if (down) {
             y += currentFallSpeed;
             if (currentFallSpeed < maxFallSpeed) {
-                currentFallSpeed += .5;
+                currentFallSpeed += .3;
             }
         }
         if (!down) {
-            currentFallSpeed = .1;
+            currentFallSpeed = .3;
         }
 
     }
